a0aacf9cf43feb9119fb0a9af52b9f0734a7a323
usuario <usuario@10.22.143.14>
2015-11-12 09:38:35 +0000

    version2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -9,8 +9,11 @@ public class Kata5 {
 
     public static void main(String[] args) {
         try {
-            Class.forName("org.sqlite.JDBC");
-            Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+            //Class.forName("org.sqlite.JDBC");
+            //Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
+            
             System.out.println("Opened database successfully");
 
             Statement stmt = c.createStatement();
